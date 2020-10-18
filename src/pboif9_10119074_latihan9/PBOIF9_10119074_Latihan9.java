0f08e484f1265aa777197a4841910e6316ca4496
HandrianRivaldi74 <handrianr28@gmail.com>
2020-10-18 14:33:52 +0700

    Latihan9

---
+++ src/pboif9_10119074_latihan9/PBOIF9_10119074_Latihan9.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pboif9_10119074_latihan9;
+
+/**
+ *
+ * @author Acer
+ * NAMA     : Handrian Rivaldi
+ * KELAS    : PBOIF2
+ * NIM      : 10119074
+ * Deskrifsi : Teks
+ */
+public class PBOIF9_10119074_Latihan9 {
+    
+    public static void main(String[] args) {
+        char c;
+        char tab ='\t';
+        char newline = '\n';
+        c = 'A';
+        System.out.println("Hasil" + newline + "Chart " + tab + "berisi " + c);
+    }
+    
+}
